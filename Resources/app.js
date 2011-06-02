cc94336d5408dfffbe6eb44d0549bdc2950d235c
Ronald Aai <ronald.aai@maxitech.cn>
2011-06-02 13:42:38 +0800

    updated all project files

---
+++ Resources/app.js
@@ -0,0 +1,305 @@
+// create tab group
+var imsi;
+var keys;
+var tf1;
+var tf2;
+var xhr2 = Titanium.Network.createHTTPClient();
+var toolActInd = Titanium.UI.createActivityIndicator();
+var username;
+var password;
+
+var tabGroup = Titanium.UI.createTabGroup();
+
+var win1 = Titanium.UI.createWindow({  
+    title:'Me-Chat',
+    backgroundColor:'#fff',
+	tabBarHidden: true  
+});
+
+var win2 = Titanium.UI.createWindow({  
+    title:'Settings',
+    backgroundColor:'#fff'
+});
+
+var tab1 = Titanium.UI.createTab({  
+    icon:'KS_nav_views.png',
+    title:'Me-Chat',
+    window:win1
+});
+
+var tab2 = Titanium.UI.createTab({  
+    icon:Titanium.UI.iPhone.SystemIcon.MORE,
+    title:'Settings',
+    window:win2
+});
+
+var a = Titanium.UI.createAlertDialog({
+	title:'Alert Test',
+	message:'Hello World'
+});
+
+//
+// ACTIVITY INDICATOR (TOOLBAR)
+//
+
+function PopUp(messageStr)
+{
+	a.message = messageStr;
+	a.show();	
+}
+
+
+function addRow(rowTitle,rowType,rowHeader,rowFooter)
+{
+	var row = Ti.UI.createTableViewRow({height:50});
+
+	switch (rowType) {
+		case 1:
+		tf1 = Titanium.UI.createTextField({
+			color:'#336699',
+			height:35,
+			top:7,
+			left:100,
+			width:190,
+			hintText:'joe.cody@me-chat.com',
+			clearButtonMode:true,
+			keyboardType:Titanium.UI.KEYBOARD_ASCII,
+			returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
+			borderStyle:Titanium.UI.INPUT_BORDERSTYLE_NONE,
+			autocapitalization:Titanium.UI.TEXT_AUTOCAPITALIZATION_NONE,
+			clearOnEdit:false,
+			autocorrect:false
+		});
+		row.add(tf1);
+		break;
+		case 2:
+		tf2 = Titanium.UI.createTextField({
+			color:'#336699',
+			height:35,
+			top:7,
+			left:100,
+			width:190,
+			keyboardType:Titanium.UI.KEYBOARD_ASCII,
+			returnKeyType:Titanium.UI.RETURNKEY_DEFAULT,
+			clearButtonMode:true,
+			borderStyle:Titanium.UI.INPUT_BORDERSTYLE_NONE,
+			autocapitalization:Titanium.UI.TEXT_AUTOCAPITALIZATION_NONE,
+			passwordMask:true,
+			clearOnEdit:false,
+			autocorrect:false
+		});
+		row.add(tf2);		
+		break;
+	}
+
+	if (rowHeader != '') {
+		row.header = rowHeader;
+	}
+
+	if (rowFooter != '') {
+		row.footer = rowFooter;
+	}
+	row.title = rowTitle;
+	row.selectionStyle = Ti.UI.iPhone.TableViewCellSelectionStyle.NONE;
+	row.className = 'control';
+	return row;
+}
+
+function StatusBar(myMessage,Action){
+	
+	switch (Action) {
+		case 1:
+			toolActInd.style = Titanium.UI.iPhone.ActivityIndicatorStyle.PLAIN;
+			toolActInd.font = {fontFamily:'Helvetica Neue', fontSize:15,fontWeight:'bold'};
+			toolActInd.color = 'white';
+			toolActInd.message = myMessage;
+			toolActInd.align = 'left';
+			win1.setToolbar([toolActInd],{animated:true});
+			toolActInd.show();
+			break;
+		case 2:
+			toolActInd.hide();
+			win1.setToolbar(null,{animated:true});
+			break;
+		}
+}
+
+function getAccountInfo()
+{
+	var db = Titanium.Database.install('db.sqlite','dbaccinfo');
+	var rows = db.execute('select distinct username,password,imsi,keys from account_info order by lastupdated;');
+
+	while (rows.isValidRow()) {
+	    username = rows.fieldByName('username');
+	    password = rows.fieldByName('password');	
+	    imsi = rows.fieldByName('imsi');
+	    keys = rows.fieldByName('keys');
+	    rows.next();
+	}
+	rows.close();
+		
+	return username+','+password+','+imsi+','+keys;
+		
+}
+
+function updateAccountInfo(username,password,imsi,keys) 
+{  	
+	StatusBar('Updating ...',1);
+	
+	var currentTime = new Date();
+	var month = currentTime.getMonth() + 1;
+	var day = currentTime.getDate();
+	var year = currentTime.getFullYear();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	var myTime = year+''+month+''+day+''+hours+''+minutes;
+
+	 var db = Titanium.Database.install('db.sqlite','dbaccinfo');
+	 var tryData = db.execute('DELETE from account_info');
+     var theData = db.execute('INSERT INTO account_info (lastupdated, username, password, imsi, keys) VALUES ("'+myTime+'","'+username+'","'+password+'", "'+imsi+'", "'+keys+'")');
+     theData;
+
+	StatusBar('Updated ...',1);
+  
+};
+
+function Login(url,username,password)
+{
+
+var parameter = Ti.Utils.base64encode(username + ',' + password);	
+
+//PopUp('Username:'+username+'\nPassword:'+password+'\n'+'Parameter:'+parameter);
+
+xhr2.open("GET","http://" + url + "?action=DEVICE.LOGIN&parameter="+parameter);
+xhr2.send();
+
+};
+
+xhr2.onload = function()
+{
+    var http_resp = "";
+	var http_resp_arr=[];
+    
+	http_resp = this.responseText;
+	//PopUp(http_resp);
+	http_resp_arr = http_resp.split(",");
+	imsi = http_resp_arr[3];
+    keys = http_resp_arr[4];
+	
+	if (imsi == "0") {
+		PopUp('Username/Password Invalid!');
+		StatusBar('',2);
+	} else {
+		StatusBar('Updating Info...',1);
+		var win = Titanium.UI.createWindow({
+            imsi:imsi,
+            keys:keys,
+			url:'menu.js',
+			title:'Friend-List'
+		});
+		updateAccountInfo(tf1.value,tf2.value,imsi,keys);
+		tab1.open(win,{animated:true});
+		StatusBar('',2);
+	}
+};
+
+xhr2.onerror = function(e)
+{
+	StatusBar(e.error,1);
+
+};
+
+// create table view data object
+var data = [];
+var savedAccountInfo = getAccountInfo();
+var savedAccountInfo_Arr = savedAccountInfo.split(",");
+
+var savedUsername = savedAccountInfo_Arr[0];
+var savedPassword = savedAccountInfo_Arr[1];
+var savedImsi = savedAccountInfo_Arr[2];
+var savedKeys = savedAccountInfo_Arr[3];
+
+data[0] = addRow("Username ",1,"Sign-in with your account","release v0.0.1");
+data[1] = addRow("Password ",2,"","");
+
+tf1.addEventListener('focus', function(e)
+{
+	toolActInd.hide();
+});
+
+tf2.addEventListener('focus', function(e)
+{
+	toolActInd.hide();
+});
+
+
+tf1.addEventListener('return', function(e)
+{
+		if (e.value == '') {
+			PopUp('username is required!');
+			tf1.focus();
+		} else {
+			tf2.focus();
+		}
+});
+
+tf2.addEventListener('return', function(e)
+{
+		if (e.value == '') {
+			PopUp('password is required!');
+			tf2.focus();
+		} else {
+				StatusBar('Signing-In...',1);
+				Login('lapp-id.545k.com/livenote/chat_lite.asp',tf1.value,tf2.value);
+		}
+});
+
+
+
+
+
+// create table view
+var tableViewOptions = {
+		data:data,
+		style:Titanium.UI.iPhone.TableViewStyle.GROUPED
+	};
+
+var tableview = Titanium.UI.createTableView(tableViewOptions);
+
+// create table view event listener
+win1.addEventListener('focus', function()
+{
+	Ti.API.info('window focus fired');
+});
+
+// add table view to the window
+win1.add(tableview);
+
+if (savedUsername != 'undefined') {
+	tf1.value = savedUsername;
+	tf2.value = savedPassword;
+	
+	//alert(savedImsi+'\n'+savedKeys);
+
+	var win = Titanium.UI.createWindow({
+        imsi:savedImsi,
+        keys:savedKeys,
+		url:'menu.js',
+		title:'Friend-List'
+	});
+	tab1.open(win,{animated:true});
+
+}
+
+//
+//  add tabs
+//
+tabGroup.addTab(tab1);  
+tabGroup.addTab(tab2);  
+
+
+// open tab group
+tabGroup.open();
+
+
+
---
+++ Resources/menu.js
@@ -0,0 +1,356 @@
+var xhr2 = Titanium.Network.createHTTPClient();
+var win = Titanium.UI.currentWindow;
+var toolActInd = Titanium.UI.createActivityIndicator();
+
+
+var tableView;
+var data=[];
+var retry = 0;
+var imsi = win.imsi;
+var keys = win.keys;
+win.barColor = '#385292';
+
+
+var a = Titanium.UI.createAlertDialog({
+	title:'Alert Test',
+	message:'Hello World'
+});
+
+//
+// CREATE SEARCH BAR
+//
+var searchBar = Titanium.UI.createSearchBar({
+	barColor:'#385292',
+	showCancel:false
+});
+
+searchBar.addEventListener('return', function(e)
+{
+searchBar.blur();
+});
+searchBar.addEventListener('cancel', function(e)
+{
+searchBar.blur();
+});
+
+function PopUp(messageStr)
+{
+	a.message = messageStr;
+	a.show();	
+}
+
+function StatusBar(myMessage,Action){
+	
+	switch (Action) {
+		case 1:
+			toolActInd.style = Titanium.UI.iPhone.ActivityIndicatorStyle.PLAIN;
+			toolActInd.font = {fontFamily:'Helvetica Neue', fontSize:15,fontWeight:'bold'};
+			toolActInd.color = 'white';
+			toolActInd.message = myMessage;
+			toolActInd.align = 'left';
+			win.setToolbar([toolActInd],{animated:true});
+			toolActInd.show();
+			break;
+		case 2:
+			toolActInd.hide();
+			win.setToolbar(null,{animated:true});
+			break;
+		}
+};
+
+function insertContacts(con_imsi,username,status_msg,avatar,lastupdated) {  
+  
+	var db = Titanium.Database.install('db.sqlite','dbaccinfo');  
+	var numfound = 0;
+	var myquery = 'SELECT count(imsi) as numfound from contacts where imsi like "'+con_imsi+'"';
+	var tmpData = db.execute(myquery);	
+	numfound = tmpData.fieldByName('numfound');
+	tmpData.close();
+	
+	//PopUp(myquery+'\nResult:['+numfound+']');
+		
+	if (numfound == "0") {
+	 //PopUp('OK!');
+     var theData = db.execute('INSERT INTO contacts (imsi, username, status_msg, avatar, lastupdated) VALUES ("'+con_imsi+'","'+username+'", "'+status_msg+'", "'+avatar+'", "'+lastupdated+'")');    
+     theData;    
+     StatusBar('Added '+username,1);
+	}
+
+};
+
+// create first row
+var row = Ti.UI.createTableViewRow();
+row.backgroundColor = '#576996';
+row.selectedBackgroundColor = '#385292';
+row.height = 40;
+var clickLabel = Titanium.UI.createLabel({
+	text:'Click different parts of the row',
+	color:'#fff',
+	textAlign:'center',
+	font:{fontSize:14},
+	width:'auto',
+	height:'auto'
+});
+row.className = 'header';
+row.add(clickLabel);
+data.push(row);
+
+cachedImageView = function(imageDirectoryName, url, imageViewObject)
+{
+// Grab the filename
+var filename = url.split('/');
+filename = filename[filename.length - 1];
+// Try and get the file that has been previously cached
+var file = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, imageDirectoryName, filename);
+
+if (file.exists()) {
+// If it has been cached, assign the local asset path to the image view object.
+imageViewObject.image = file.nativePath;
+} else {
+// If it hasn't been cached, grab the directory it will be stored in.
+var g = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, imageDirectoryName);
+if (!g.exists()) {
+// If the directory doesn't exist, make it
+g.createDirectory();
+};
+
+// Create the HTTP client to download the asset.
+var xhr = Ti.Network.createHTTPClient();
+
+xhr.onload = function() {
+if (xhr.status == 200) {
+// On successful load, take that image file we tried to grab before and
+// save the remote image data to it.
+file.write(xhr.responseData);
+// Assign the local asset path to the image view object.
+imageViewObject.image = file.nativePath;
+};
+};
+
+// Issuing a GET request to the remote URL
+xhr.open('GET', url);
+// Finally, sending the request out.
+xhr.send();
+};
+};
+
+
+// create update row (used when the user clicks on the row)
+function createUpdateRow(text)
+{
+	var updateRow = Ti.UI.createTableViewRow();
+	updateRow.backgroundColor = '#13386c';
+	updateRow.selectedBackgroundColor = '#13386c';
+
+	// add custom property to identify this row
+	updateRow.isUpdateRow = true;
+	var updateRowText = Ti.UI.createLabel({
+		color:'#fff',
+		font:{fontSize:20, fontWeight:'bold'},
+		text:text,
+		width:'auto',
+		height:'auto'
+	});
+	updateRow.className = 'updated_row';
+	updateRow.add(updateRowText);
+	return updateRow;
+}
+
+function rtrim(str, chars) {
+	chars = chars || "\\s";
+	chars = str.replace(new RegExp("[" + chars + "]+$", "g"), "");
+	chars = chars.substring(0,chars.length-1);
+	return chars;
+}
+
+
+function pad(number) {
+   
+     return (number < 10 ? '0' : '') + number;
+   
+}
+
+function getFriendList(url,imsi){
+
+StatusBar('Loading friendlist...'+imsi,1);
+
+xhr2.open("GET","http://" + url + "?action=BBFRIENDLIST&imsi="+imsi);
+xhr2.send();
+
+}
+
+xhr2.onload = function()
+{
+	
+    var http_resp = "";
+	var http_resp_arr=[];
+	var pos = 0;
+	http_resp = this.responseText;
+	//http_resp_arr = http_resp.split(",");
+	
+	//PopUp(http_resp);
+	
+	var currentTime = new Date();
+	var month = currentTime.getMonth() + 1;
+	var day = currentTime.getDate();
+	var year = currentTime.getFullYear();
+	var hours = currentTime.getHours();
+	var minutes = currentTime.getMinutes();
+	
+	
+	var myTime = year+''+pad(month)+''+pad(day)+''+pad(hours)+''+pad(minutes);
+	
+	for (pos = 0; pos < http_resp.length; pos+=252) {
+	 user_status = http_resp.substring(pos,pos+1);
+	 username = rtrim(http_resp.substring(pos+1,pos+22));
+	 con_imsi = rtrim(http_resp.substring(pos+22,pos+39));
+	 comments = rtrim(http_resp.substring(pos+39,pos+120));
+	 avatar_ext = rtrim(http_resp.substring(pos+120,pos+124));
+	 avatar = rtrim(http_resp.substring(pos+124,pos+251));
+	 //PopUp('['+pos+']\n'+'['+username+']\n'+'['+con_imsi+']\n'+'['+comments+']\n'+'['+avatar+']\n');
+	 insertContacts(con_imsi,username,comments,avatar,myTime);
+	};
+		
+	StatusBar('',2);
+	
+		
+	//data.push(addRow(username,photos,comments));
+
+};
+
+xhr2.onerror = function(e)
+{
+	//retry++;
+	//if (retry >= 3) {
+	//	getFriendList('lapp-id.545k.com/livenote/chat_lite.asp',imsi);		
+	//	StatusBar('Retrying ['+retry+']...',1);
+	//} else {
+	//	StatusBar(e.error,1);
+	//}
+	StatusBar('error loading...',1);
+};
+
+getFriendList('lapp-id.545k.com/livenote/chat_lite.asp',imsi);
+
+var db = Titanium.Database.install('db.sqlite','dbaccinfo');
+var rows = db.execute('select * from contacts order by username;');
+
+var currentRow = null;
+var currentRowIndex = null;
+
+while (rows.isValidRow()) {
+    username = rows.fieldByName('username');
+    avatar = rows.fieldByName('avatar');	
+    comments = rows.fieldByName('status_msg');
+    lastseen = rows.fieldByName('lastupdated');
+    lastseen_day = lastseen.substring(6,8);
+	lastseen_hrmin = lastseen.substring(8,13);
+
+	var row = Ti.UI.createTableViewRow();
+	row.selectedBackgroundColor = '#fff';
+	row.height = 100;
+	row.className = 'datarow';
+	row.clickName = 'row';
+	
+	var photo = Ti.UI.createImageView({
+		image:avatar,
+		defaultImage:'user.png',
+		top:5,
+		left:10,
+		width:50,
+		height:50,
+		clickName:'photo'
+	});
+	
+	cachedImageView('cache', avatar, photo);
+	
+	row.add(photo);
+	
+	var user = Ti.UI.createLabel({
+		color:'#576996',
+		font:{fontSize:16,fontWeight:'bold', fontFamily:'Arial'},
+		left:70,
+		top:2,
+		height:30,
+		width:200,
+		clickName:'user',
+		text:username
+	});
+	row.filter = user.text;
+	row.add(user);	
+
+	var fontSize = 16;
+	if (Titanium.Platform.name == 'android') {
+		fontSize = 14;
+	}
+	var comment = Ti.UI.createLabel({
+		color:'#222',
+		font:{fontSize:fontSize,fontWeight:'normal', fontFamily:'Arial'},
+		left:70,
+		top:21,
+		height:50,
+		width:200,
+		clickName:'comment',
+		text:comments
+	});
+	row.add(comment);
+
+	var calendar = Ti.UI.createView({
+		backgroundImage:'dates/'+lastseen_day+'.png',
+		bottom:2,
+		left:70,
+		width:32,
+		clickName:'calendar',
+		height:32
+	});
+	row.add(calendar);
+
+	var button = Ti.UI.createView({
+		backgroundImage:'commentButton.png',
+		top:35,
+		right:5,
+		width:36,
+		clickName:'button',
+		height:34
+	});
+	row.add(button);
+	
+	var date = Ti.UI.createLabel({
+		color:'#999',
+		font:{fontSize:13,fontWeight:'normal', fontFamily:'Arial'},
+		left:105,
+		bottom:5,
+		height:20,
+		width:100,
+		clickName:'date',
+		text:'lastseen '+ lastseen_hrmin
+	});
+	row.add(date);	
+	
+	data.push(row);
+
+    rows.next();
+}
+rows.close();
+
+
+tableView = Titanium.UI.createTableView({
+	data:data,
+	search:searchBar,
+	filterAttribute:'filter',
+	backgroundColor:'white'
+});
+
+tableView.addEventListener('click', function(e)
+{
+	Ti.API.info('table view row clicked - source ' + e.source);
+	// use rowNum property on object to get row number
+	var rowNum = e.index;
+	var updateRow = createUpdateRow('You clicked on the '+e.source.clickName);
+	tableView.updateRow(rowNum,updateRow,{animationStyle:Titanium.UI.iPhone.RowAnimationStyle.LEFT});
+});
+
+
+
+win.add(tableView);
+
